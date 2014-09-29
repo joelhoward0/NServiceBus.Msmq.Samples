fc0a59436e460afa50ab44876d8a1669a408bf69
Simon Cropp <simon.cropp@gmail.com>
2014-09-29 13:22:49 +1000

    update error handling sample

---
+++ ErrorHandling/WithSLR/MyMessageHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using NServiceBus;
+
+public class MyMessageHandler : IHandleMessages<MyMessage>
+{
+    static readonly ConcurrentDictionary<Guid, string> Last = new ConcurrentDictionary<Guid, string>();
+
+    public IBus Bus { get; set; }
+
+    public void Handle(MyMessage message)
+    {
+        Console.WriteLine("ReplyToAddress: {0} MessageId:{1}", Bus.CurrentMessageContext.ReplyToAddress, Bus.CurrentMessageContext.Id);
+        var numOfRetries = Bus.CurrentMessageContext.Headers[Headers.Retries];
+
+        if (numOfRetries != null)
+        {
+            string value;
+            Last.TryGetValue(message.Id, out value);
+
+            if (numOfRetries != value)
+            {
+                Console.WriteLine("This is second level retry number {0}", numOfRetries);
+                Last.AddOrUpdate(message.Id, numOfRetries, (key, oldValue) => numOfRetries);
+            }
+        }
+
+        throw new Exception("An exception occurred in the handler.");
+    }
+}
---
+++ ErrorHandling/WithSLR/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NServiceBus;
+
+static class Program
+{
+    static void Main()
+    {
+        var configuration = new BusConfiguration();
+        configuration.EndpointName("Samples.ErrorHandling.WithSLR");
+        configuration.UseSerialization<JsonSerializer>();
+        configuration.UsePersistence<InMemoryPersistence>();
+        configuration.EnableInstallers();
+        using (var bus = Bus.Create(configuration))
+        {
+            bus.Start();
+            Console.WriteLine("Press any key to send a message that will throw an exception.");
+            Console.WriteLine("To exit, press Ctrl + C");
+
+            while (true)
+            {
+                Console.ReadLine();
+                var m = new MyMessage
+                {
+                    Id = Guid.NewGuid()
+                };
+                bus.SendLocal(m);
+            }
+        }
+    }
+}
